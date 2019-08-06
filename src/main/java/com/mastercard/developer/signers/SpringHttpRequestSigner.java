f783075b2a73be0bba3c6c279e60aac73bf76521
Ahmed Hosny <filemeidea@gmail.com>
2019-08-06 15:52:52 +0000

    Added a signer for Spring HTTP requests

---
+++ src/main/java/com/mastercard/developer/signers/SpringHttpRequestSigner.java
@@ -0,0 +1,41 @@
+package com.mastercard.developer.signers;
+
+import com.mastercard.developer.oauth.OAuth;
+
+import java.nio.charset.Charset;
+import java.security.PrivateKey;
+
+import org.springframework.http.HttpMessage;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+/**
+ * Utility class for signing Spring RestTemplate requests.
+ */
+public class SpringHttpRequestSigner extends AbstractSigner {
+    
+  public SpringHttpRequestSigner(String consumerKey, PrivateKey signingKey) {
+    super(consumerKey, signingKey);
+  }
+
+  public void sign(HttpRequest request, byte[] bytes) {
+    HttpHeaders headers = request.getHeaders();
+    Charset charset = getCharset(headers);
+    String payload = new String(bytes, charset);
+    String authHeader = OAuth.getAuthorizationHeader(request.getURI(), request.getMethod().toString(), payload, charset, consumerKey, signingKey);
+    headers.add(OAuth.AUTHORIZATION_HEADER_NAME, authHeader);
+  }
+  
+  private static Charset getCharset(HttpHeaders headers){
+    Charset defaultCharset = Charset.defaultCharset();
+    MediaType contentType = headers.getContentType();
+    if(contentType != null){
+      Charset charset = contentType.getCharset();
+      if(charset != null){
+        return charset;
+      }
+    }
+    return defaultCharset;
+  }
+}
---
+++ src/test/java/com/mastercard/developer/signers/SpringHttpRequestSignerTest.java
@@ -0,0 +1,94 @@
+package com.mastercard.developer.signers;
+
+import com.mastercard.developer.test.TestUtils;
+
+import org.springframework.http.HttpMessage;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+
+import java.net.URI;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.security.PrivateKey;
+
+public class SpringHttpRequestSignerTest {
+    
+    private static final HttpMethod method = HttpMethod.POST;
+    private static final String body = "{\"foo\":\"bar\"}";
+    private static final String consumerKey = "Some key";
+    
+    private PrivateKey signingKey;
+	private URI uri;
+    private HttpHeaders headers;
+    private HttpRequest request;
+    
+    @Before 
+    public void initialize() throws Exception {
+    
+        signingKey = TestUtils.getTestSigningKey();
+        uri = new URI("https://api.mastercard.com/service");
+        headers = new HttpHeaders();
+        request = new HttpRequest() {
+            public HttpMethod getMethod(){
+                return method;
+            }
+            public String getMethodValue(){
+                return getMethod().toString();
+            }
+            public URI getURI(){
+                return uri;
+            }
+            public HttpHeaders getHeaders(){
+                return headers;
+            }
+        };
+    }
+
+    @Test
+    public void testSign_ShouldAddOAuth1HeaderToPostRequest() throws Exception {
+
+        // WHEN
+        SpringHttpRequestSigner instanceUnderTest = new SpringHttpRequestSigner(consumerKey, signingKey);
+        instanceUnderTest.sign(request, body.getBytes());
+
+        // THEN
+        String authorizationHeaderValue = headers.getFirst(HttpHeaders.AUTHORIZATION);
+        Assert.assertNotNull(authorizationHeaderValue);
+    }
+    
+    @Test
+    public void testSign_ShouldAddOAuth1HeaderToPostRequestWithCharset() throws Exception {
+
+        // GIVEN
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+
+        // WHEN
+        SpringHttpRequestSigner instanceUnderTest = new SpringHttpRequestSigner(consumerKey, signingKey);
+        instanceUnderTest.sign(request, body.getBytes());
+
+        // THEN
+        String authorizationHeaderValue = headers.getFirst(HttpHeaders.AUTHORIZATION);
+        Assert.assertNotNull(authorizationHeaderValue);
+    }
+
+    @Test
+    public void testSign_ShouldAddOAuth1HeaderToPostRequestWithInvalidCharset() throws Exception {
+
+        // GIVEN
+        headers.setContentType(MediaType.APPLICATION_PDF);
+
+        // WHEN
+        SpringHttpRequestSigner instanceUnderTest = new SpringHttpRequestSigner(consumerKey, signingKey);
+        instanceUnderTest.sign(request, body.getBytes());
+
+        // THEN
+        String authorizationHeaderValue = headers.getFirst(HttpHeaders.AUTHORIZATION);
+        Assert.assertNotNull(authorizationHeaderValue);
+    }
+
+}
