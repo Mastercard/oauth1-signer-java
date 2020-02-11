c9799d20753accb2c1e94070d4eeda9feb47145d
Aufauvre, Jean-Alexis <jean-alexis.aufauvre@outlook.com>
2020-02-11 09:54:45 +0000

    Fixed #17 ("no such provider: SunJSSE" in IBM Websphere Runtime environment)

--- src/main/java/com/mastercard/developer/utils/AuthenticationUtils.java
+++ src/main/java/com/mastercard/developer/utils/AuthenticationUtils.java
@@ -18,8 +18,8 @@ public final class AuthenticationUtils {
      */
     public static PrivateKey loadSigningKey(String pkcs12KeyFilePath,
                                             String signingKeyAlias,
-                                            String signingKeyPassword) throws IOException, NoSuchProviderException, KeyStoreException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
-        KeyStore pkcs12KeyStore = KeyStore.getInstance("PKCS12", "SunJSSE");
+                                            String signingKeyPassword) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
+        KeyStore pkcs12KeyStore = KeyStore.getInstance("PKCS12");
         pkcs12KeyStore.load(new FileInputStream(pkcs12KeyFilePath), signingKeyPassword.toCharArray());
         return (PrivateKey) pkcs12KeyStore.getKey(signingKeyAlias, signingKeyPassword.toCharArray());
     }
