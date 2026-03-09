00698eb4f14bc2036e81cc35a12bf3a17e5211d0
joseph-neeraj <joseph.neeraj@gmail.com>
2026-03-09 17:01:29 +0000

    update SignatureMethod details in the README (#71)
    * update SignatureMethod details in the README
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- src/main/java/com/mastercard/developer/oauth/SignatureMethod.java
+++ src/main/java/com/mastercard/developer/oauth/SignatureMethod.java
@@ -9,7 +9,25 @@ import java.security.spec.PSSParameterSpec;
  */
 public enum SignatureMethod {
 
+    /**
+     * RSA-SHA256 signature method.
+     * <p>Uses the {@code SHA256withRSA} JCA algorithm (RSASSA-PKCS1-v1_5 with SHA-256).</p>
+     *
+     * @see <a href="https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#signature-algorithms">Java Signature Algorithms</a>
+     */
     RSA_SHA256("SHA256withRSA", "RSA-SHA256", null),
+
+    /**
+     * RSA-PSS signature method.
+     * <p>Uses the {@code RSASSA-PSS} JCA algorithm with the following parameters:</p>
+     * <ul>
+     *   <li>Digest: SHA-256</li>
+     *   <li>Mask generation function: MGF1 with SHA-256</li>
+     *   <li>Salt length: 32 bytes</li>
+     * </ul>
+     *
+     * @see <a href="https://docs.oracle.com/en/java/javase/17/docs/specs/security/standard-names.html#signature-algorithms">Java Signature Algorithms</a>
+     */
     RSA_PSS_SHA256("RSASSA-PSS", "RSA-PSS", new PSSParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256, 32, 1));
 
     /** JCA signature algorithm name. */
